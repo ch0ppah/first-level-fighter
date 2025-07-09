7dc42b5de59991481d31e9669b8497e54a6a36c1
ch0ppah <nick0suna76@gmail.com>
2025-07-08 20:08:15 -0400

    successfully ran test_generate.py

--- src/generate.py
+++ src/generate.py
@@ -21,15 +21,15 @@ def generate():
     return [species, name, stats, stat_mods, chosen_class, abilities, equipment]
 
 def generate_name():
-    with open('../content/names.csv') as file:
+    with open('content/names.csv', encoding='utf-8-sig') as file:
         names = list(map(lambda line: line.strip().split(','), file))
     
-    num_species = len(names[0]) - 1
-    num_names = len(names[0][0]) - 1
-    species_index = random.randrange(0, num_species)
-    name_index = random.randrange(1,num_names)
+    num_species = len(names)
+    species_index = random.randrange(num_species)
+    num_names = len(names[species_index])
+    name_index = random.randrange(1, num_names)
 
-    species = names[species_index]
+    species = names[species_index][0]
     name = names[species_index][name_index]
 
     print(f"You are {name}, the {species}.")
@@ -51,7 +51,7 @@ def generate_stats():
     return stats
 
 def roll_stat():
-    lowest_roll, highest_roll = 1, 6
+    lowest_roll, highest_roll = 1, 7
     die_1 = random.randrange(lowest_roll, highest_roll)
     die_2 = random.randrange(lowest_roll, highest_roll)
     die_3 = random.randrange(lowest_roll, highest_roll)
@@ -59,37 +59,39 @@ def roll_stat():
 
     # Puts all die rolls into a list
     top_3_dice = sorted([die_1, die_2, die_3, die_4])[1:]
-
-    return top_3_dice[1] + top_3_dice[2] + top_3_dice[3]
+    # print("-----------DEBUGGING MESSAGES-----------")
+    # print(f"top_3_dice[0]: {top_3_dice[0]}")
+    # print(f"top_3_dice[1]: {top_3_dice[1]}")
+    # print(f"top_3_dice[2]: {top_3_dice[2]}")
+    # print(f"TOTAL: {top_3_dice[0] + top_3_dice[1] + top_3_dice[2]}")
+    return top_3_dice[0] + top_3_dice[1] + top_3_dice[2]
 
 def statmod(stat):
     return (stat - 10) // 2
 
 def get_equipment(chosen_class):
-    with open('../content/equipment.csv') as file:
+    with open('content/equipment.csv', encoding='utf-8-sig') as file:
         equipment = list(map(lambda line: line.strip().split(','), file))
     
     return next((row for row in equipment if row[0].startswith(chosen_class)), None)[1:]
 
 def pick_class(classes):
-    chosen_class = input("Please select a class by typing in the class you would like to use for your stat spread.")
-    if chosen_class == 'QUIT':
-        print("First Level Fighter terminated")
-        exit()
-    elif chosen_class.lower().capitalize in classes:
-        return chosen_class
+    chosen_class = input_check_quit("Please type in the class you would like to use with your stat spread: ")
+    print()
+    if chosen_class.lower().capitalize() in classes:
+        return chosen_class.lower().capitalize()
     else:
         print("Invalid class")
-        pick_class()
+        return pick_class(classes)
 
 def get_abilities(chosen_class):
-    with open('../content/abilities.csv') as file:
+    with open('content/abilities.csv', encoding='utf-8-sig') as file:
         abilities = list(map(lambda line: line.strip().split(','), file))
 
     return next((row for row in abilities if row[0].startswith(chosen_class)), None)[1:]
 
 def show_classes():
-    with open('../content/abilities.csv') as file:
+    with open('content/abilities.csv', encoding='utf-8-sig') as file:
         classes = list(map(lambda line: line.strip().split(',')[0], file))
     print("Here are the classes you can select:")
     for clas in classes:
@@ -101,98 +103,80 @@ def show_classes():
 # If anyone sees this function and wants to share a better way to accomplish this please let me know
 def allocate_asi(stats):
     STR, DEX, CON, INT, WIS, CHA = 0, 1, 2, 3, 4, 5
-    asi = input()
-    if asi == 'QUIT':
-        print("First Level Fighter terminated")
-        exit()
-    asi = asi.split()
-    if len(asi) == 2:
-        match asi[0].upper():
-            case "STR":
-                stats[STR] += 2
-            case "DEX":
-                stats[DEX] += 2
-            case "CON":
-                stats[CON] += 2
-            case "INT":
-                stats[INT] += 2
-            case "WIS":
-                stats[WIS] += 2
-            case "CHA":
-                stats[CHA] += 2
-            case _:
-                raise ValueError(f"invalid value given: {asi[0]} is not valid stat name")
+    stat_names = ["STR", "DEX", "CON", "INT", "WIS", "CHA"]
+    asis = input_check_quit().split()
+    
+    if len(set(asis)) < 2 or len(set(asis)) > 3:
+        print("Improper amount of unique inputs.")
+        print("Please enter 2-3 valid stat names (STR, DEX, CON, INT, WIS, CHA)")
+        print()
+        return allocate_asi(stats)
+
+    for stat in set(asis):
+        if stat.upper() not in stat_names:
+            print(f"'{stat}' is not a valid stat name SET CHECK")
+            print('Please enter 2-3 valid stat names (STR, DEX, CON, INT, WIS, CHA)')
+            print()
+            return allocate_asi(stats)
+
+    if len(asis) == 2:
+        for i in range(2):
+            increment_amount = 2
+            if i == 1:
+                increment_amount = 1
+            match asis[i].upper():
+                case "STR":
+                    stats[STR] += increment_amount
+                case "DEX":
+                    stats[DEX] += increment_amount
+                case "CON":
+                    stats[CON] += increment_amount
+                case "INT":
+                    stats[INT] += increment_amount
+                case "WIS":
+                    stats[WIS] += increment_amount
+                case "CHA":
+                    stats[CHA] += increment_amount
+                case _:
+                    print(f"'{asis[i]}' is not valid stat name LEN 2 CHECK")
+                    print('Please enter 2-3 valid stat names (STR, DEX, CON, INT, WIS, CHA)')
+                    print()
+                    allocate_asi(stats)
+        return stats
             
-        match asi[1].upper():
-            case "STR":
-                stats[STR] += 1
-            case "DEX":
-                stats[DEX] += 1
-            case "CON":
-                stats[CON] += 1
-            case "INT":
-                stats[INT] += 1
-            case "WIS":
-                stats[WIS] += 1
-            case "CHA":
-                stats[CHA] += 1
-            case _:
-                raise ValueError(f"invalid value given: {asi[1]} is not valid stat name")
-            
-    elif len(asi) == 3:
-        match asi[0].upper():
-            case "STR":
-                stats[STR] += 1
-            case "DEX":
-                stats[DEX] += 1
-            case "CON":
-                stats[CON] += 1
-            case "INT":
-                stats[INT] += 1
-            case "WIS":
-                stats[WIS] += 1
-            case "CHA":
-                stats[CHA] += 1
-            case _:
-                raise ValueError(f"invalid value given: {asi[0]} is not valid stat name")
-            
-        match asi[1].upper():
-            case "STR":
-                stats[STR] += 1
-            case "DEX":
-                stats[DEX] += 1
-            case "CON":
-                stats[CON] += 1
-            case "INT":
-                stats[INT] += 1
-            case "WIS":
-                stats[WIS] += 1
-            case "CHA":
-                stats[CHA] += 1
-            case _:
-                raise ValueError(f"invalid value given: {asi[1]} is not valid stat name")
-            
-        match asi[2].upper():
-            case "str":
-                stats[0] += 1
-            case "dex":
-                stats[1] += 1
-            case "con":
-                stats[2] += 1
-            case "int":
-                stats[3] += 1
-            case "wis":
-                stats[4] += 1
-            case "cha":
-                stats[5] += 1
-            case _:
-                raise ValueError(f"invalid value given: {asi[2]} is not valid stat name")
-    else:
-        print("Invalid stat names entered")
-        allocate_asi(stats)
+    elif len(asis) == 3:
+        increment_amount = 1
+        for stat in asis:
+            match stat.upper():
+                case "STR":
+                    stats[STR] += increment_amount
+                case "DEX":
+                    stats[DEX] += increment_amount
+                case "CON":
+                    stats[CON] += increment_amount
+                case "INT":
+                    stats[INT] += increment_amount
+                case "WIS":
+                    stats[WIS] += increment_amount
+                case "CHA":
+                    stats[CHA] += increment_amount
+                case _:
+                    print(f"'{asis}' is not valid stat name LEN 3 CHECK")
+                    print('Please enter 2-3 valid stat names (STR, DEX, CON, INT, WIS, CHA)')
+                    print()
+                    allocate_asi(stats)
     return stats
 
 def prompt_for_asi():
     print("Choose which ability scores to increase.")
     print("Typing a pair of abbreviated stat names ('STR DEX', for example) will grant +2 to the first stat and +1 to the second.")
     print("Alternatively, typing three abbreviated stat names ('STR DEX CON', for example) will instead grant +1 to each.")
+
+def input_check_quit(prompt=""):
+    inp = input(prompt)
+    if 'QUIT' in inp:
+        print("First Level Fighter Terminated")
+        print()
+        exit()
+    else:
+        return inp
--- src/test_generate.py
+++ src/test_generate.py
@@ -10,25 +10,32 @@ from generate import (
     show_classes,
     allocate_asi,
 )
+
+# TODO: RUN THE TESTS!!!!
+
 class TestGenerate(unittest.TestCase):
     def test_generate(self):
-        with open('content/names.csv') as file:
+        with open('content/names.csv', encoding='utf-8-sig') as file:
             names = list(map(lambda line: line.strip().split(','), file))
-        with open('content/abilities.csv') as file:
-            classes = list(map(lambda line: line.strip().split(',')[0], file))
-        with open('content/equipment.csv') as file:
+        with open('content/abilities.csv', encoding='utf-8-sig') as file:
+            classes = list(map(lambda line: line.strip().split(','), file))
+        with open('content/equipment.csv', encoding='utf-8-sig') as file:
             equipment_list = list(map(lambda line: line.strip().split(','), file))
         
-        for _ in range(10):
+        for _ in range(1):
             species, name, stats, stat_mods, chosen_class, abilities, equipment = generate()
-            self.assertIn(species, names, f"{species} is not a species in names.csv")
-            self.assertIn(name, names, f"{name} is not a name in names.csv")
+            self.assertIn(species, [row[0] for row in names], f"{species} is not a species in names.csv")
+            species_row = next(row for row in names if row[0] == species)
+            self.assertIn(name, species_row[1:], f"{name} is not a valid name for species {species} in names.csv")
             for stat in stats:
                 self.assertGreaterEqual(stat, 3, f"Rolled stat ({stat}) is not greater than or equal to 3")
                 self.assertLessEqual(stat, 18, f"Rolled stat ({stat}) is not less than or equal to 18")
             for stat_mod in stat_mods:
                 self.assertGreaterEqual(stat_mod, -4, f"Rolled stat ({stat_mod}) is not greater than or equal to -4")
                 self.assertLessEqual(stat_mod, 4, f"Rolled stat ({stat_mod}) is not less than or equal to 4")
-            self.assertIn(chosen_class, classes, f"{chosen_class} is not in abilities.csv")
-            self.assertIn(abilities, classes, f"Error pulling abilities for the {chosen_class} class")
-            self.assertIn(equipment, equipment_list, f"Error pulling equipment for the {chosen_class} class")
+            classes_row = [row[0] for row in classes]
+            self.assertIn(chosen_class, classes_row, f"{chosen_class} is not in {classes_row}")
+            abilities_row = next(row for row in classes if row[0] == chosen_class)[1:]
+            self.assertEqual(abilities, abilities_row, f"Error pulling abilities for the {chosen_class} class")
+            equipment_row = next(row for row in equipment_list if row[0] == chosen_class)[1:]
+            self.assertEqual(equipment, equipment_row, f"Error pulling equipment for the {chosen_class} class")
